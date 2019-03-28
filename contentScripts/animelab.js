10ce032a0dcbab25ba734ab3d438c8dbd7dcc635
met4000 <nathsharwor@hotmail.com>
2019-03-28 21:31:24 +0800

    Move card modification to function

--- contentScripts/animelab.js
+++ contentScripts/animelab.js
@@ -30,23 +30,26 @@ for (var i = 0; i < cards.length; i++) {
   xhttp.onreadystatechange = new Function(`
     if (this.readyState == 4 && this.status == 200) {
       var i = ` + i + `;
-      var show = JSON.parse(this.responseText).results[0];
-      
-      var link = document.createElement("div");
-      link.innerHTML = '<a href="' + show.url + '" class="btn btn-watch-instantly btn-full-width"><span class="glyphicon"><img src="https://cdn.myanimelist.net/images/faviconv5.ico"></span>View on MAL</a>';
-      link = link.firstChild;
-      details[i].insertBefore(link, details[i].children[2]);
-
-      if (secondaryDetails[i] != null) {
-        var score = document.createElement("div");
-        score.classList = "block-full-width";
-        score.innerHTML = '<h5 style="display: inline;">MAL Rating</h5><span style="float: right;">' + parseScore(show.score) + '</span>';
-        secondaryDetails[i].insertBefore(score, secondaryDetails[i].children[2]);
-      } else {
-        console.info("Unable to get card back for " + show.title + " (i = " + i + ") - Is it a premium episode card?");
-      }
+      modifyCard(i, JSON.parse(this.responseText).results[0]);
     }
   `);
   xhttp.open("GET", "https://api.jikan.moe/v3/search/anime?q=" + title);
   xhttp.send();
+}
+
+
+function modifyCard(i, show) {
+  var link = document.createElement("div");
+  link.innerHTML = '<a href="' + show.url + '" class="btn btn-watch-instantly btn-full-width"><span class="glyphicon"><img src="https://cdn.myanimelist.net/images/faviconv5.ico"></span>View on MAL</a>';
+  link = link.firstChild;
+  details[i].insertBefore(link, details[i].children[2]);
+
+  if (secondaryDetails[i] != null) {
+    var score = document.createElement("div");
+    score.classList = "block-full-width";
+    score.innerHTML = '<h5 style="display: inline;">MAL Rating</h5><span style="float: right;">' + parseScore(show.score) + '</span>';
+    secondaryDetails[i].insertBefore(score, secondaryDetails[i].children[2]);
+  } else {
+    console.info("Unable to get card back for " + show.title + " (i = " + i + ") - Is it a premium episode card?");
+  }
 }
