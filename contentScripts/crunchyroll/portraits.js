4366343c97649275b0f8e8ec85aec2783dd2f86f
met4000 <nathsharwor@hotmail.com>
2019-04-08 22:37:30 +0800

    Add title title Also some internal stuff

--- contentScripts/crunchyroll/portraits.js
+++ contentScripts/crunchyroll/portraits.js
@@ -1,18 +1,20 @@
 console.log("Running crunchyroll portrait injection script...");
 
 
-var portraits = document.getElementsByClassName("portrait-element");
+var portraits = document.getElementsByClassName("wrapper container-shadow");
 
 for (var i = 0; i < portraits.length; i++) {
-  var title = portraits[i].querySelector("span.series-title.block.ellipsis").innerText;
+  var title = portraits[i].querySelector("a > span.series-title.block.ellipsis").innerText;
 
   MAL.getByTitle(title, new Function(`modifyPortrait(` + i + `, arguments[0]);`));
 }
 
 
 function modifyPortrait(i, show) {
-  var info = portraits[i].querySelector("span.series-data.block.ellipsis");
-  info.innerHTML = '<span>' + info.innerHTML + '</span><span style="float: right;">' + parseScore(show.score) + '</span>';
+  var info = portraits[i].querySelector("a > span.series-data.block.ellipsis");
+  info.innerHTML = '<span>' + info.innerHTML + '</span><span title="' + show.title + '" style="float: right;">' + parseScore(show.score) + '</span>';
 
-  // var link = document.createElement("div");
+  // var olink = portaits[i].querySelector("button");
+  // var link = olink.cloneNode(); // shouldn't clone it - WIP
+  // olink.style += "top: 140px;";
 }
